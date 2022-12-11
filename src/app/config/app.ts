d55ff73edd12a06f2ceb4a92e2f97c08eb1e9b36
Paola-Ali-Gonzales <agp2017905@est.univalle.edu>
2022-12-11 09:10:09 -0400

    Actualizacion

--- src/app/config/app.ts
+++ src/app/config/app.ts
@@ -1,1 +1,20 @@
-export const BASE_ENDPOINT = 'http://localhost:8090/api/almacenes';
+
+//ANTES 
+
+//export const BASE_ENDPOINT = 'http://localhost:8090/api/almacenes';
+
+// AHORA
+
+export const BASE_ENDPOINT = 'http://ec2-50-19-136-71.compute-1.amazonaws.com:8090/api/almacenes';
+
+
+
+
+
+
+
+
+
+
+
+
